cd0e16807ddd03f045188c6f38e20c643510d727
James Booth <james.booth@microchip.com>
2022-04-19 11:08:54 +0100

    "LAUNCH v1.0 (beta) of app with executable

--- PYTHONDATA/dataLoggerGUI.py
+++ PYTHONDATA/dataLoggerGUI.py
@@ -1,7 +1,13 @@
 from distutils import command
+from posixpath import basename
 import tkinter as tk
 from tkinter.filedialog import askopenfile, askopenfilename, asksaveasfilename
 from lineCounter import run_line_counter
+import time
+
+
+timestr = time.strftime("%d/%m/%Y-%H:%M:%S")
+print (timestr)
 
 filepath = ''
 
@@ -24,6 +30,7 @@ def log_data():
     "Log OEE data to the database"
     global filepath
     run_line_counter(filepath)
+    lbl_value["text"] = f"Data for uploaded at {timestr}"
     
     window.title(f"OEE Datalogger {filepath} ")
 
@@ -37,9 +44,11 @@ txt_edit =tk.Text(window)
 fr_buttons = tk.Frame(window)
 btn_open = tk.Button(fr_buttons, text ="Open", command=open_file)
 btn_log = tk.Button(fr_buttons, text="Log Data", command=log_data)
+lbl_value = tk.Label(master=window, text="Waiting for Data...")
 
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_log.grid(row=1, column=0, sticky="ew", padx=5)
+lbl_value.grid(row=1, column=1, sticky="ew", padx=5)
 
 fr_buttons.grid(row=0, column=0, sticky="ns")
 txt_edit.grid(row=0, column=1, sticky="nsew")
@@ -47,3 +56,5 @@ txt_edit.grid(row=0, column=1, sticky="nsew")
 
 
 window.mainloop()
+
+###To do: improve GUI, add microchip logo as an image, add text pop up when data is logged succsessfully###
