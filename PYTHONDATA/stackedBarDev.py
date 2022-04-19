f63389e3db5c599a395c02441df6454392e3060a
James Booth <james.booth@microchip.com>
2022-04-19 11:58:51 +0100

    began work on OEE bar plot, dropped duplicates and created time series dataframe

--- PYTHONDATA/stackedBarDev.py
+++ PYTHONDATA/stackedBarDev.py
@@ -1,11 +1,22 @@
 import plotly.express as px
 import pandas as pd
 
+#To do: create a function that will read the basename from datalog.csv and convert to date-time
+#To do: join dataframes on the date-time column
+
+datalog = pd.read_csv(f'C:\\vs_code\\OEE_DASHBOARD\\DATABASE\\datalog.csv')
+# print(datalog)
+
+df = pd.DataFrame(datalog)
+
+df = df.drop_duplicates(subset=['Date','Availability','Performance','Quality','OEE'], keep=False)
+print(df)
+print(df.shape)
+
+bDate = pd.bdate_range(start='3/1/2022', end='3/31/2022') #business date range March 2022#
+print(bDate)
 
 
-datalog = pd.read_csv(f'C:\\Users\\M68153\\OneDrive - Microchip Technology Inc\\Desktop\\OEE_DASHBOARD\\DATABASE\\datalog.csv')
-print(datalog)
-
-
-fig = px.bar(datalog, x="Date", y= "OEE", color="Availability", title="OEE Historical")
-fig.show()
+#fig = px.bar(df, x="Date", y= "OEE", title="OEE Historical")
+#fig = px.bar(df, x="Date", y= "OEE", color="Availability", title="OEE Historical")
+#fig.show()
