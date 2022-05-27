744e54a0e15cf967a983a00aefa4d870db5344f2
James Booth <james.booth@microchip.com>
2022-05-27 10:34:55 +0100

    populated updatePlots.py with function calls

---
+++ PYTHONDATA/updatePlots.py
@@ -0,0 +1,16 @@
+from plotlyAvailabilityGauge import run_plotly_Availability
+from plotlyPerformanceGauge import run_plotly_Performance
+from plotlyQualityGauge import run_plotly_Quality
+from plotlyOEEGauge import run_plotly_OEE
+from plotlyTimeSeries import run_plotly_TimeSeries
+
+def __main__():
+
+    print("Run Update Plots Function")
+    run_plotly_Availability
+    run_plotly_Performance
+    run_plotly_Quality
+    run_plotly_OEE
+    run_plotly_TimeSeries
+
+__main__()
