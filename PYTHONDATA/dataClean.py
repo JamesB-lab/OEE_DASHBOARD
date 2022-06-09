2e6f31bb81a06344683d54f86299d0a26a7e9ddc
James Booth <james.booth@microchip.com>
2022-06-09 17:41:30 +0100

    fixed dataClean.py now correctly drops bmc row from a single column unformatted dataframe also exports .ram file to new folder structure in CAL_Common

--- PYTHONDATA/dataClean.py
+++ PYTHONDATA/dataClean.py
@@ -1,49 +1,22 @@
-from ast import If
-from turtle import color
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import os.path
-from datetime import date
 
 
-file = '22_03.ram'
+file = '09_05.ram'
 
-# rawData = pd.read_fwf(f'C:\\Users\\M68153\\OneDrive - Microchip Technology Inc\\Desktop\\Coding\\evo_log_data\\DA5_Copy\\BMCDROPTEST\\{file}', colspecs=[(1,23), (25,33)], names=['Col_1', 'Col_2'])
-#print(rawData)
+basename = os.path.basename(file) #ok
+print(f'basename: {basename}') #ok
 
-rawData = pd.read_fwf(f'C:\\Users\\M68153\\OneDrive - Microchip Technology Inc\\Desktop\\Coding\\evo_log_data\\DA5_Copy\\BMCDROPTEST\\{file}', colspecs=[(1,23), (25,33)], names=['Col_1', 'Col_2'])
-
+rawData = pd.read_fwf(f'C:\\Users\\M68153\\OneDrive - Microchip Technology Inc\\Desktop\\Coding\\evo_log_data\\DA5_Copy\\BMCDROPTEST\\{file}', colspecs=[(0,142)], names=['Col_1'],)
 
 df = pd.DataFrame(rawData)
-# print(df)
-# print(df.shape)
 
 
-# print(df.index)
-
-# for label in df.index:
-#     print(f'\'{label}\'')
-
-ignore = ['bmc']
-#df = df[df.Col_1.isin(ignore) == False]
-
-
+ignore = ['bmc', 'bmc       0          0          0          0          0          0          0          0          0          0          0']
 df = df[df.Col_1.isin(ignore) == False]
-
-
-# for i in range(len(df)) :
-# #   print(df.iloc[i, 0])
-#   if df.iloc[i, 0] == 'bmc':
-#      print('True')
-#      #df = df[df.Col_1.isin(ignore) == False]
-#       #df = df.drop(df.loc['bmc', 0], axis=0, index=0)
-#       #df = df.drop(df.loc[df['Col_1']=='bmc'].index, inplace=True)
-#       #df = df.drop('bmc')
-    
-
 
 print(f'df new = {df}')
 
-#df.to_csv(r'c:\data\pandas.txt', header=None, index=None, sep=' ', mode='a')
-df.to_csv(r'C:\\vs_code\\OEE_DASHBOARD\\DATABASE\\cleaning.txt', index=False, mode='a', header=False)
+
+df.to_csv(f'P:\\OEE_Dashboard\\Cleaned_Data_Output\\{basename}', index=False, mode='a', header=False)
+#df.to_csv(f'C:\\vs_code\\OEE_DASHBOARD\\DATABASE\\{basename}', index=False, mode='a', header=False)
