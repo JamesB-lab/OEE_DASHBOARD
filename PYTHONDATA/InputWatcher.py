cc4bb9c86136593d085f6c6cf66fa98547053a60
James Booth <james.booth@microchip.com>
2022-06-10 11:25:19 +0100

    created fixed and finished InputWatcher.py which will trigger dataClean.py when a new file is added to Raw_Data_Input in CAL_Common: still risks that could crash the system i.e. non .ram files

---
+++ PYTHONDATA/InputWatcher.py
@@ -0,0 +1,47 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from dataClean import run_Data_Clean
+
+# This function is just a dummy example, replace with real OEE functions
+# def run_OEE(path):
+#     print(f'Running OEE on {path}')
+
+def run_Data_Clean_Trigger(path):
+     print(f'Running dataClean on {path}')
+     run_Data_Clean(path)
+
+
+
+class JamesEventHandler(FileSystemEventHandler):
+ # We're only interested in created events, so we only need to implement this method
+    def on_created(self, event):
+# In general, there's no guarantee that the file will be good since it's user input
+# Hence, we protect ourselves with a try-except block
+        try:
+            if event.is_directory == False:
+                run_Data_Clean_Trigger(event.src_path)
+                # print('True')
+                # print(event)
+        except BaseException as err:
+            print(err)
+
+
+if __name__ == "__main__":
+    path = 'P:\\OEE_Dashboard\\Raw_Data_Input'
+    # We create a new instance of our custom handler
+    event_handler = JamesEventHandler()
+    # We create a new watchdog observer
+    observer = Observer()
+    # We tell the observer to watch a 'path' recursively and use the 'event_handler'
+    observer.schedule(event_handler, path, recursive=True)
+    # We start the observer
+    observer.start()
+    try:
+        # We need an infinite loop here so the program doesn't terminate
+        while True:
+            time.sleep(1)
+    finally:
+        # We graciously stop the observer and wait for it to finish
+        observer.stop()
+        observer.join()
--- PYTHONDATA/dataClean.py
+++ PYTHONDATA/dataClean.py
@@ -2,21 +2,25 @@ import pandas as pd
 import os.path
 
 
-file = '09_05.ram'
+#file = '09_05.ram'
 
-basename = os.path.basename(file) #ok
-print(f'basename: {basename}') #ok
-
-rawData = pd.read_fwf(f'C:\\Users\\M68153\\OneDrive - Microchip Technology Inc\\Desktop\\Coding\\evo_log_data\\DA5_Copy\\BMCDROPTEST\\{file}', colspecs=[(0,142)], names=['Col_1'],)
-
-df = pd.DataFrame(rawData)
+def run_Data_Clean(path):
+    print(f'Running Data_Clean on {path}')
 
 
-ignore = ['bmc', 'bmc       0          0          0          0          0          0          0          0          0          0          0']
-df = df[df.Col_1.isin(ignore) == False]
+    basename = os.path.basename(path) #ok
+    print(f'basename: {basename}') #ok
 
-print(f'df new = {df}')
+    rawData = pd.read_fwf(path, colspecs=[(0,142)], names=['Col_1'],)
+
+    df = pd.DataFrame(rawData)
 
 
-df.to_csv(f'P:\\OEE_Dashboard\\Cleaned_Data_Output\\{basename}', index=False, mode='a', header=False)
-#df.to_csv(f'C:\\vs_code\\OEE_DASHBOARD\\DATABASE\\{basename}', index=False, mode='a', header=False)
+    ignore = ['bmc', 'bmc       0          0          0          0          0          0          0          0          0          0          0']
+    df = df[df.Col_1.isin(ignore) == False]
+
+    print(f'df new = {df}')
+
+
+    df.to_csv(f'P:\\OEE_Dashboard\\Cleaned_Data_Output\\{basename}', index=False, mode='a', header=False)
+    #df.to_csv(f'C:\\vs_code\\OEE_DASHBOARD\\DATABASE\\{basename}', index=False, mode='a', header=False)
