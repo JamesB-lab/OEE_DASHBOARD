3642c8b05dc0675f4353a01dc04e5bb004ec2b52
James Booth <james.booth@microchip.com>
2022-06-10 11:48:52 +0100

    created cleanedDataWatcher.py

--- PYTHONDATA/chartStudioUpload.py
+++
@@ -1,8 +0,0 @@
-import chart_studio
-
-username = 'james.booth' # your username
-api_key = 'cCmuSWNFC4GOKnshMCr2' # your api key - go to profile > settings > regenerate key
-chart_studio.tools.set_credentials_file(username=username, api_key=api_key)
-
-import chart_studio.plotly as py
-py.plot(fig, filename = 'gdp_per_cap', auto_open=True)
