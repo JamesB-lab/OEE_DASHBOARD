e56cd278b36de9f6dda64f80ec89f4c38fd218eb
James Booth <james.booth@microchip.com>
2022-06-16 11:24:22 +0100

    updated watcherWrapper.py to handle adjustment made in previous commit for watcher files

--- PYTHONDATA/watcherWrapper.py
+++ PYTHONDATA/watcherWrapper.py
@@ -1,13 +1,24 @@
 from ramFilterWatcher import run_ramFilterWatcher
 from InputWatcher import run_inputWatcher
 from cleanedDataWatcher import run_cleanedDataWatcher
+import time
 
 def __main__():
 
     print("Running watcher programs")
-    run_ramFilterWatcher()
-    run_inputWatcher()
-    run_cleanedDataWatcher()
-    
 
+    ramFileObserver = run_ramFilterWatcher()
+    inputObserver = run_inputWatcher()
+    cleanedDataObserver = run_cleanedDataWatcher()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        ramFileObserver.stop()
+        inputObserver.stop()
+        cleanedDataObserver.stop()
+    ramFileObserver.join()
+    inputObserver.join()
+    cleanedDataObserver.join()
+ 
 __main__()
