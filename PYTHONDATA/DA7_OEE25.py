443c5d536bf19881e00a440c1a35382879c483ed
James Booth <james.booth@microchip.com>
2022-07-07 11:20:10 +0100

    updated DA7_OEE25.py for new machine config. Tested, works ok

--- PYTHONDATA/DA7_OEE25.py
+++ PYTHONDATA/DA7_OEE25.py
@@ -16,139 +16,139 @@ from datetime import date
 from DA7_updatePlots import run_update_plots
 
 
+def run_OEE_25(path):
+
+    print('Running OEE25')
+
+    path = 'C:\\Users\\M68153\\OneDrive - Microchip Technology Inc\\Desktop\\Coding\\testEnv\\07_07.ram'
+
+    basename = os.path.basename(path)
+
+    basename = basename.replace('.ram', '')
+    print(f'basename: {basename}')
+
+    ##Import raw data as fixed width file (fwf)###
+    data = pd.read_fwf(path, skiprows=3, skipfooter=3, colspecs=[(0,23), (23,37), (37,-1)], names=['Category', 'System 1', 'System 2'])
+    print(f'OEE25 Data{data}')
 
 
-print('Running OEE25')
+    ###AVAILABILITY Convert raw data to dataFrame, subset for timedelata###
+    dftd = pd.DataFrame(data)
+    dftd = dftd.drop(axis=0, index =[9,10,11, 12, 13, 14, 15, 16, 17])
+    print(f'OEE25 dftd data: {dftd}')
 
-path = 'C:\\Users\\M68153\\OneDrive - Microchip Technology Inc\\Desktop\\Coding\\testEnv\\07_07.ram'
+    # ###AVAILABILITY Cast Types convert from Object to string/timedelta###
+    dftd['Category'] = dftd['Category'].astype('string') #Error#
+    dftd['System 1'] = pd.to_timedelta(dftd['System 1'])
+    dftd['System 2'] = pd.to_timedelta(dftd['System 2'])
+    print(f'OEE25 dftd data post cast: {dftd}')
 
-basename = os.path.basename(path)
+    # ###AVAILABILITY Add extra column for plotting, convert timedelata to float64###
+    dftd['System 1'] = dftd['System 1'] / pd.Timedelta(hours =1)
+    dftd['System 2'] = dftd['System 2'] / pd.Timedelta(hours =1)
 
-basename = basename.replace('.ram', '')
-print(f'basename: {basename}')
+    print(f'OEE25 dftd: {dftd}')
+    print(f'OEE25 dftd dtypes: {dftd.dtypes}')
 
-##Import raw data as fixed width file (fwf)###
-data = pd.read_fwf(path, skiprows=3, skipfooter=3, colspecs=[(0,23), (23,37), (37,-1)], names=['Category', 'System 1', 'System 2'])
-print(f'OEE25 Data{data}')
-#print(f'OEE25 Data Shape{df.shape(data)}')
+    ###AVAILABILITY OEE Availability Calcs###
+    ###AVAILABILITY Availability = Actual Production Time / Possible Production Time * 100###
 
-# ###AVAILABILITY Convert raw data to dataFrame, subset for timedelata###
-# dftd = pd.DataFrame(data)
-# dftd = dftd.drop(axis=0, index =[9,10,11, 12, 13, 14, 15, 16, 17])
-# print(f'OEE25 dftd data: {dftd}')
-
-# ###AVAILABILITY Cast Types convert from Object to string/timedelta###
-# dftd['Category'] = dftd['Category'].astype('string') #Error#
-# dftd['System 1'] = pd.to_timedelta(dftd['System 1'])
-# dftd['System 2'] = pd.to_timedelta(dftd['System 2'])
-# print(f'OEE25 dftd data post cast: {dftd}')
-
-# ###AVAILABILITY Add extra column for plotting, convert timedelata to float64###
-# dftd['System 1'] = dftd['System 1'] / pd.Timedelta(hours =1)
-# dftd['System 2'] = dftd['System 2'] / pd.Timedelta(hours =1)
-
-# print(f'OEE25 dftd: {dftd}')
-# print(f'OEE25 dftd dtypes: {dftd.dtypes}')
-
-# ###AVAILABILITY OEE Availability Calcs###
-# ###AVAILABILITY Availability = Actual Production Time / Possible Production Time * 100###
-
-# possibleProd = dftd.loc[dftd.index[2], 'System 2']
+    possibleProd = dftd.loc[dftd.index[2], 'System 1']
 
 
-# actualProd = dftd.loc[dftd.index[5], 'System 2']
+    actualProd = dftd.loc[dftd.index[5], 'System 1']
 
 
-# availability = actualProd / possibleProd
-# print(f'OEE25 possibleProd = {possibleProd}')
-# print(f'OEE25 actualProd = {actualProd}')
-# print(f'OEE25 availability = {availability}')
-
-
-
-# ###QUALITY Subset for Quality Statistics ###
-
-# dfq = pd.DataFrame(data)
-# print(f'OEE25 rawdataframe: {dfq}')
-# dfq = dfq.drop(axis=0, index =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
-# print(f'OEE25 dfq dataset: {dfq}')
-
-
-# ###QUALITY Cast Types convert from Object to string/int64###
-# dfq['Category'] = dfq['Category'].astype('string')
-# dfq['System 2'] = dfq['System 2'].astype('int')
+    availability = actualProd / possibleProd
+    print(f'OEE25 possibleProd = {possibleProd}')
+    print(f'OEE25 actualProd = {actualProd}')
+    print(f'OEE25 availability = {availability}')
 
 
 
-# ###QUALITY OEE Quality Calcs###
-# ###QUALITY Quality = Good Count/Total Count * 100 == Placed/Picked#
+    ###QUALITY Subset for Quality Statistics ###
 
-# pickup = dfq.loc[dfq.index[0], 'System 2']
-# place = dfq.loc[dfq.index[1], 'System 2']
+    dfq = pd.DataFrame(data)
+    print(f'OEE25 rawdataframe: {dfq}')
+    dfq = dfq.drop(axis=0, index =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+    print(f'OEE25 dfq dataset: {dfq}')
 
-# quality = place / pickup
-# print(f'OEE25 pickup = {pickup}')
-# print(f'OEE25 place = {place}')
-# print(f'OEE25 quality = {quality}')
 
-# ###PERFORMANCE Subset for Performance Statistics ###
-
-# data2 = pd.read_fwf(path, skiprows=22, skipfooter=0,   colspecs=[(0,13), (14,24), (25, 37), (38, 49), (50, 60), (61, 71), (72, 83), (83, 90), (91, 100), (101, 112), (112, 124), (125,-1)], names=['ComponentStatistics', 'Total', 'Useable', 'Reject', 'Inked', 'Pos-Error', 'Vac-Error', 'AM-Err1', 'AM-Err2', 'AM-Err3', 'AM-Err4', 'InspErr' ])
-# dfp = pd.DataFrame(data2)
-# print(f'OEE25 data2 = {data2}')
+    ###QUALITY Cast Types convert from Object to string/int64###
+    dfq['Category'] = dfq['Category'].astype('string')
+    dfq['System 1'] = dfq['System 1'].astype('int')
 
 
 
-# ###PERFORMANCE OEE Quality Calcs###
+    ###QUALITY OEE Quality Calcs###
+    ###QUALITY Quality = Good Count/Total Count * 100 == Placed/Picked#
 
+    pickup = dfq.loc[dfq.index[0], 'System 1']
+    place = dfq.loc[dfq.index[1], 'System 1']
 
-# actualOut = dfp.loc[dfp.index[1], 'Total']
-# actualOut = int(actualOut)
-# PossibleOut = 7010
-# print(f'Actual Out: {actualOut}')
-# print(type(actualOut))
+    quality = place / pickup
+    print(f'OEE25 pickup = {pickup}')
+    print(f'OEE25 place = {place}')
+    print(f'OEE25 quality = {quality}')
 
+    ###PERFORMANCE Subset for Performance Statistics ###
 
-
-# performance = actualOut / PossibleOut #error
-
-
-
-# ###FINAL OEE CACLULATION###
-
-# OEE = availability * performance * quality * 100
-
-# print(f'Availbability = {availability}')
-# print(f'Performance = {performance}')
-# print(f'Quality = {quality}')
-# print(f'OEE = {OEE} %')
+    data2 = pd.read_fwf(path, skiprows=22, skipfooter=0,   colspecs=[(0,13), (14,24), (25, 37), (38, 49), (50, 60), (61, 71), (72, 83), (83, 90), (91, 100), (101, 112), (112, 124), (125,-1)], names=['ComponentStatistics', 'Total', 'Useable', 'Reject', 'Inked', 'Pos-Error', 'Vac-Error', 'AM-Err1', 'AM-Err2', 'AM-Err3', 'AM-Err4', 'InspErr' ])
+    dfp = pd.DataFrame(data2)
+    print(f'OEE25 data2 = {data2}')
 
 
 
-# ###Save Results to csv###
-
-# ###Create DataFrame of the results: availability, performance, quality, OEE###
-# ramYear = str(datetime.datetime.now().year)
-# date_text = basename.replace('_','/')
-# date_text = date_text.replace('.RAM','') + '/' + ramYear
-# date_time = pd.to_datetime(date_text, format='%d/%m/%Y').date()
-# print(f' Date_Text = {date_text}')
-# print(f' Date_Time = {date_time}')
-
-# dict = {'Datetime': date_time, 'RamDate': basename, 'Availability': availability*100, 'Performance': performance*100, 'Quality': quality*100, 'OEE': OEE, 'Machine': 'DA7'}
+    ###PERFORMANCE OEE Quality Calcs###
 
 
-# resultsDF = pd.DataFrame.from_dict(dict, orient='index')
-# resultsTransp = resultsDF.transpose()
+    actualOut = dfp.loc[dfp.index[1], 'Total']
+    actualOut = int(actualOut)
+    PossibleOut = 7010
+    print(f'Actual Out: {actualOut}')
+    print(type(actualOut))
 
 
-# ###Write to csv###
 
-# resultsTransp.to_csv(r'P:\\OEE_Dashboard\\Data\\datalog.csv', index=False, mode='a', header=False)
+    performance = actualOut / PossibleOut #error
+    print(performance)
 
 
-# print('DA7_Program complete')
-# print('DA7_Updating plots')
-# #run_update_plots()
+    ###FINAL OEE CACLULATION###
+
+    OEE = availability * performance * quality * 100
+
+    print(f'Availbability = {availability}')
+    print(f'Performance = {performance}')
+    print(f'Quality = {quality}')
+    print(f'OEE = {OEE} %')
+
+
+
+    ###Save Results to csv###
+
+    ###Create DataFrame of the results: availability, performance, quality, OEE###
+    ramYear = str(datetime.datetime.now().year)
+    date_text = basename.replace('_','/')
+    date_text = date_text.replace('.RAM','') + '/' + ramYear
+    date_time = pd.to_datetime(date_text, format='%d/%m/%Y').date()
+    print(f' Date_Text = {date_text}')
+    print(f' Date_Time = {date_time}')
+
+    dict = {'Datetime': date_time, 'RamDate': basename, 'Availability': availability*100, 'Performance': performance*100, 'Quality': quality*100, 'OEE': OEE, 'Machine': 'DA7'}
+
+
+    resultsDF = pd.DataFrame.from_dict(dict, orient='index')
+    resultsTransp = resultsDF.transpose()
+
+
+    ###Write to csv###
+
+    resultsTransp.to_csv(r'P:\\OEE_Dashboard\\Data\\datalog.csv', index=False, mode='a', header=False)
+
+
+    print('DA7_Program complete')
+    print('DA7_Updating plots')
+    run_update_plots()
 
 
